e9dc631cef1d7f43558274fa8fab393a9cc478fc
Santoshgs13 <81846783+Santoshgs13@users.noreply.github.com>
2021-05-10 10:22:38 +0530

    New

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		System.out.println("\n Hello, ");
+		System.out.println("\n Welcome to Treosoftit ");
+		System.out.println("\n Welcome to Waycool ");
+	}
+
+}
